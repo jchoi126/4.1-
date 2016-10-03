8104d8c31aa7b9afe0a615dd19d54714c14fe9c7
Johnnie Choi <johnniechoi@Johnnies-MacBook-Pro.local>
2016-10-03 17:56:53 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+(function($){
+  $(function(){
+  "use strict";
+
+// $('.subject').on('click', function(){
+//   event.preventDefault();
+//   $(this).children('li').hide();
+// });
+
+$( ".subject" ).click(function() {
+  $(this).children('li').toggle ("slow");
+});
+
+$(document).ready(function() {
+   $('.subject').hover(
+      function () {
+         $(this).css({"background-color":"grey"});
+      },
+      function () {
+         $(this).css({"background-color":"white"});
+      }
+   );
+});
+
+
+// $(".subject").hover(function() {
+//   event.preventDefault();
+//   $(this).addClass("blue");
+// }, function() {
+//   $(this).removeClass("blue");
+// });
+
+
+
+  });
+}(jQuery));
